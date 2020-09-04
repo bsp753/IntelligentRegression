95eb24e7c425bfbd8df73cdf9314367bfd4d498d
redara <redara@deloitte.com>
2020-09-04 21:36:31 +0530

    Updated Agent code for defect #2356

--- src/main/java/com/insta/application/model/Agent.java
+++ src/main/java/com/insta/application/model/Agent.java
@@ -91,6 +91,21 @@ public class Agent
 		this.mobileno = mobileno;
 	}
 	
+	public ArrayList<testDataObject> fetchTestScenarios(@RequestParam String startDate,@RequestParam String endDate) throws Exception 
+	{
+		LoginDataObject dataObject = new LoginDataObject();
+		HashMap<String, String> propertiesMap=service.loadPropertiesFile();
+		System.out.println(propertiesMap);
+		ArrayList<testDataObject> fetchScenariosSet = service.fetchScenarios(startDate, endDate, propertiesMap.get("user"), propertiesMap.get("password"), 
+					propertiesMap.get("moduleMappingCSVFilePath"),propertiesMap.get("scenarioCSVFilePath"),
+					propertiesMap.get("url"));
+		/*
+		 * model.put("todosLength", fetchScenariosSet.size()); model.put("todos",
+		 * fetchScenariosSet);
+		 */
+		//dataObject.setTestScenariosList(fetchScenariosSet);
+		return fetchScenariosSet;
+	}
 	
 
 }
